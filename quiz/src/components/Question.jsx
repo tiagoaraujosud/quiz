fac3cec77f48e0295bb168af882ff047f40c0b7a
TIAGO MATIAS DE ARAUJO <tiagoaraujosud@gmail.com>
2023-05-31 15:37:14 -0300

    create onClick function to change questions and change css configuration

--- quiz/src/components/Question.jsx
+++ quiz/src/components/Question.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import { QuizContext } from "../context/quiz";
 
+import "./Question.css";
+
 const Question = () => {
     const [quizState, dispatch] = useContext(QuizContext);
     const currentQuestion = quizState.questions[quizState.currentQuestion];
@@ -13,7 +15,9 @@ const Question = () => {
         <div id="options-container">
             <p>Opções</p>
         </div>
-        <button>Continuar</button>
+        <button onClick={() => dispatch({ type: "CHANGE_QUESTION" })}>
+            Continuar
+        </button>
     </div>
 }
 
--- quiz/src/context/quiz.jsx
+++ quiz/src/context/quiz.jsx
@@ -28,6 +28,13 @@ const quizReducer = (state, action) => {
                 questions: reorderedQuestions,
             };
 
+        case "CHANGE_QUESTION":
+            const nextQuestion = state.currentQuestion + 1;
+            return {
+                ...state,
+                currentQuestion: nextQuestion,
+            };
+
         default:
             return state;
     }
