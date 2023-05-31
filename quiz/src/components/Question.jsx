d2c81e242fcd05b3b0be12f586660f66846ed7f2
TIAGO MATIAS DE ARAUJO <tiagoaraujosud@gmail.com>
2023-05-31 16:22:41 -0300

    Create Option component and Option css configuration

--- quiz/src/components/Question.jsx
+++ quiz/src/components/Question.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import { QuizContext } from "../context/quiz";
 
+import Option from "./Option";
+
 import "./Question.css";
 
 const Question = () => {
@@ -13,7 +15,9 @@ const Question = () => {
         </p>
         <h2>{currentQuestion.question}</h2>
         <div id="options-container">
-            <p>Opções</p>
+            {currentQuestion.options.map((option) => (
+                <Option option={option} key={option} />
+            ))}
         </div>
         <button onClick={() => dispatch({ type: "CHANGE_QUESTION" })}>
             Continuar
